56ea7035026564c9b4e8725a893bea4a2b16244c
Ngọc Lam <nglamvn2911@gmail.com>
2022-09-09 22:56:39 +0700

    Add warning for development build

--- src/vndustry/utility/Utility.java
+++ src/vndustry/utility/Utility.java
@@ -1,15 +1,23 @@
 package vndustry.utility;
 
 import arc.util.CommandHandler;
+import jdk.internal.org.jline.utils.Log;
 import mindustry.mod.Plugin;
 import vndustry.utility.command.CommandRegister;
 
 public class Utility extends Plugin {
 
+    private static final boolean IS_DEVELOPMENT_BUILD = true;
+
     @Override
     public void init() {
         EventHandler.initEvents();
         Filters.initFilters();
+
+        if (IS_DEVELOPMENT_BUILD){
+            Log.warn("You are running a VndustryUtility DEVELOPMENT build !");
+            Log.warn("The build may have bugs, crash, corrupt your data");
+        }
     }
 
     @Override
